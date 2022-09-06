6c4bb156bca69732cba46c5fac1dd86ca075704f
Matheus Donadoni <62766278+MatheusDonadoni2@users.noreply.github.com>
2022-09-06 12:46:12 -0300

    Delete dist directory

--- dist/src/shared/infra/http/routes/cars.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.carsRoutes = void 0;
-const CreateCarControoller_1 = require("@modules/cars/useCases/createCars/CreateCarControoller");
-const express_1 = require("express");
-const carsRoutes = (0, express_1.Router)();
-exports.carsRoutes = carsRoutes;
-const createCarController = new CreateCarControoller_1.CreateCarController();
-carsRoutes.post('/', createCarController.handle);
